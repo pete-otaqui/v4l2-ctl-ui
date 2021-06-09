ffc452e532c33aaa1bedaf8750a264625b1c7c20
Pete Otaqui <pete@otaqui.com>
2021-06-09 11:42:59 +0100

    Added reset

--- src/public/index.js
+++ src/public/index.js
@@ -42,37 +42,45 @@ function getForm() {
 }
 
 function createSettingControl(setting, form) {
-  const containerId = `${setting.name}-container`;
-  let container = document.getElementById(containerId);
-  let label;
-  if (!container) {
-    container = document.createElement('div');
-    container.setAttribute('id', containerId);
-    container.classList.add('setting');
-    label = document.createElement('label');
-    container.appendChild(label);
-    label.setAttribute('for', setting.name);
-    label.textContent = setting.name;
-    form.appendChild(container);
-  } else {
-    label = container.querySelector('label');
-  }
   switch(setting.type) {
     case 'int':
-      createSettingControlInt(setting, container);
+      createSettingControlInt(setting, form);
       break;
     case 'bool':
-      createSettingControlBool(setting, container);
+      createSettingControlBool(setting, form);
       break;
     case 'menu':
-      createSettingControlMenu(setting, container);
+      createSettingControlMenu(setting, form);
       break;
   }
 }
 
-function createBasicControl(setting, container, type, getValue) {
-  let control = container.querySelector(type);
-  if (!control) {
+function createBasicControl(setting, form, type, getValue) {
+  console.log('createBasicControl', setting.name);
+  const containerId = `${setting.name}-container`;
+  let container = document.getElementById(containerId);
+  let meta;
+  let label;
+  let values;
+  let control;
+  let reset;
+  if (!container) {
+    container = document.createElement('div');
+    container.setAttribute('id', containerId);
+    container.classList.add('setting');
+    meta = document.createElement('div');
+    label = document.createElement('label');
+    values = document.createElement('div');
+    reset = document.createElement('button');
+    reset.innerHTML = `Reset: ${setting.default}`;
+    reset.classList.add('reset');
+    values.classList.add('values');
+    meta.appendChild(label);
+    meta.appendChild(values);
+    container.appendChild(meta);
+    label.setAttribute('for', setting.name);
+    form.appendChild(container);
+
     control = document.createElement(type);
     control.classList.add('setting-control');
     control.setAttribute('name', setting.name);
@@ -81,8 +89,23 @@ function createBasicControl(setting, container, type, getValue) {
       const value = getValue(control);
       updateSettingControl(setting.name, value);
     }, false);
+    reset.addEventListener('click', (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      updateSettingControl(setting.name, setting.default);
+    }, false);
     container.appendChild(control);
+  } else {
+    label = container.querySelector('label');
+    values = container.querySelector('.values');
+    control = container.querySelector(type);
+    reset = container.querySelector('.reset');
+    reset.parentNode.removeChild(reset);
   }
+  label.textContent = `${setting.name}`;
+  values.innerHTML = `cur: ${setting.value}, default ${setting.default}, `;
+  reset.innerHTML = `reset`;
+  values.appendChild(reset);
   if (setting.inactive) {
     control.setAttribute('readonly', true);
     control.setAttribute('disabled', true);
@@ -90,20 +113,18 @@ function createBasicControl(setting, container, type, getValue) {
     control.removeAttribute('readonly');
     control.removeAttribute('disabled');
   }
-  // control.setAttribute('readonly', setting.inactive ? true : false);
-  // control.setAttribute('disabled', setting.inactive ? true : false);
   return control;
 }
 
-function createSettingControlInt(setting, container) {
-  const control = createBasicControl(setting, container, 'input', (control) => parseInt(control.value, 10));
+function createSettingControlInt(setting, form) {
+  const control = createBasicControl(setting, form, 'input', (control) => parseInt(control.value, 10));
   control.setAttribute('type', 'range');
   control.setAttribute('min', setting.min);
   control.setAttribute('max', setting.max);
   control.setAttribute('value', setting.value);
 }
-function createSettingControlBool(setting, container) {
-  const control = createBasicControl(setting, container, 'input', (control) => control.checked ? 1 : 0);
+function createSettingControlBool(setting, form) {
+  const control = createBasicControl(setting, form, 'input', (control) => control.checked ? 1 : 0);
   control.setAttribute('type', 'checkbox');
   if (setting.value === 1) {
     control.setAttribute('checked', true);
@@ -111,8 +132,8 @@ function createSettingControlBool(setting, container) {
     control.removeAttribute('checked');
   }
 }
-function createSettingControlMenu(setting, container) {
-  const control = createBasicControl(setting, container, 'select', (control) => parseInt(control.value, 10));
+function createSettingControlMenu(setting, form) {
+  const control = createBasicControl(setting, form, 'select', (control) => parseInt(control.value, 10));
   control.innerHTML = '';
   setting.options.forEach((settingOption, index) => {
     const option = document.createElement('option');
